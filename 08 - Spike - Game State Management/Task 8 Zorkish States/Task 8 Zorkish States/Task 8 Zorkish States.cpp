2163ceff9bab6276b0a39cfd4d3f6eb231e21f39
Ryan <102564760@student.swin.edu.au>
2021-11-07 02:05:35 +1100

    Task 8 Project Setup

---
+++ 08 - Spike - Game State Management/Task 8 Zorkish States/Task 8 Zorkish States/Task 8 Zorkish States.cpp
@@ -0,0 +1,9 @@
+// Task 8 Zorkish States.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
