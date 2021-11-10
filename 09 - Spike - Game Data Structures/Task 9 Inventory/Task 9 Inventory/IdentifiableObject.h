5e60344a8698b4d17ec9fcfc5aaf9fc90dc450f8
Ryan <102564760@student.swin.edu.au>
2021-11-10 17:36:27 +1100

    Task 9 Identifiable object header

--- 09 - Spike - Game Data Structures/Task 9 Inventory/Task 9 Inventory/IdentifiableObject.h
+++ 09 - Spike - Game Data Structures/Task 9 Inventory/Task 9 Inventory/IdentifiableObject.h
@@ -1,5 +1,19 @@
 #pragma once
+#include <list>
+#include <string>
+
+using namespace std;
+
 class IdentifiableObject
 {
+private:
+	list<string> identifiers;
+public:
+	IdentifiableObject(list<string> ids);
+	~IdentifiableObject();
+
+	bool AreYou(string id);
+	string FirstId();
+	void AddIdentifier(string id);
 };
 
