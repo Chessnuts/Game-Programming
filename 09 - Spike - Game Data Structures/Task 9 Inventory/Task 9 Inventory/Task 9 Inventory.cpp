85e7e4736c4585beb7da5a51647043d54a6abb60
Ryan <102564760@student.swin.edu.au>
2021-11-11 20:53:21 +1100

    Task 9 Identifiable object test

--- 09 - Spike - Game Data Structures/Task 9 Inventory/Task 9 Inventory/Task 9 Inventory.cpp
+++ 09 - Spike - Game Data Structures/Task 9 Inventory/Task 9 Inventory/Task 9 Inventory.cpp
@@ -5,13 +5,61 @@
 #include "IdentifiableObject.h"
 #include "GameObject.h"
 
+
 using namespace std;
+
+void identifiableObjectTest()
+{
+    IdentifiableObject io({ "identifier 1", "identifier 2", "identifier 3" });
+
+    cout << io.FirstId() << endl;
+
+    if (io.AreYou("identifier 1"))
+    {
+        cout << "i am identifier 1" << endl;
+    }
+
+    if (io.AreYou("identifier 2"))
+    {
+        cout << "i am identifier 2" << endl;
+    }
+
+    if (io.AreYou("identifier 3"))
+    {
+        cout << "i am identifier 3" << endl;
+    }
+
+    if (!io.AreYou("identifier 4"))
+    {
+        cout << "i am not identifier 4" << endl;
+    }
+
+    io.AddIdentifier("identifier 4");
+    
+    if (io.AreYou("identifier 4"))
+    {
+        cout << "i am now identifier 4" << endl;
+    }
+}
+
+void GameObjectTest()
+{
+
+}
+
+void ItemTest()
+{
+
+}
+
+void InventoryTest()
+{
+
+}
 
 int main()
 {
-    IdentifiableObject io({ "1", "2", "3" });
-
-    cout << io.FirstId() << endl;
+    identifiableObjectTest();
 
     return 0;
 }
