0219690ce83fd5c62e0cf8b98b83cc45b022035c
Ryan <102564760@student.swin.edu.au>
2021-11-12 22:37:35 +1100

    Task 10 steps 2 and 3

--- 10 - Lab - File Input Output/Task 10/Task 10/Task 10.cpp
+++ 10 - Lab - File Input Output/Task 10/Task 10/Task 10.cpp
@@ -1,9 +1,40 @@
 
 #include <iostream>
 
+using namespace std;
+
+class testClass
+{
+private:
+    int anInt;
+    char aChar;
+    float aFloat;
+public:
+    testClass(int in, char ch, float fl) 
+    {
+        anInt = in;
+        aChar = ch;
+        aFloat = fl;
+    };
+    ~testClass() {};
+
+    void ShowValues()
+    {
+        cout << anInt << endl;
+        cout << aChar << endl;
+        cout << aFloat << endl;
+    };
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    int in = 6;
+    char ch = 'a';
+    float fl = 2.2;
+    testClass myClass(in, ch, fl);
+
+
+    return 0;
 }
 
 
