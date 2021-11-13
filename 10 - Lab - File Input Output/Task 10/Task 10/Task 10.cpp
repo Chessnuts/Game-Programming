dd10bbf72efa5884f2b78b430682a34c952ccfd8
Ryan <102564760@student.swin.edu.au>
2021-11-14 02:20:36 +1100

    Task 10 Reading file code and notes

--- 10 - Lab - File Input Output/Task 10/Task 10/Task 10.cpp
+++ 10 - Lab - File Input Output/Task 10/Task 10/Task 10.cpp
@@ -20,24 +20,37 @@ struct Test
 
 int main()
 {
-    int in = 6;
-    char ch = 'a';
-    float fl = 2.2;
+    int in;
+    char ch;
+    float fl;
+
+    fstream fs;
+    fs.open("test1.bin", fstream::in | fstream::binary);
+
+    fs.read(reinterpret_cast<char*>(&in), 4);
+    fs.read(reinterpret_cast<char*>(&ch), 1);
+    fs.read(reinterpret_cast<char*>(&fl), 4);
+
+    fs.close();
+
     Test myData{ in, ch, fl };
 
     myData.ShowValues();
 
-    //open in write mode
-    fstream fs;
-    fs.open("test1.bin", fstream::out | fstream::binary);
+    if (false)
+    {
+        //open in write mode
+        fstream fs;
+        fs.open("test1.bin", fstream::out | fstream::binary | fstream::trunc);
 
-    //write data
-    fs << myData.anInt;
-    fs << myData.aChar;
-    fs << myData.aFloat;
+        //write data
+        fs.write(reinterpret_cast<const char*>(&myData.anInt), sizeof(myData.anInt));
+        fs.write(reinterpret_cast<const char*>(&myData.aChar), sizeof(myData.aChar));
+        fs.write(reinterpret_cast<const char*>(&myData.aFloat), sizeof(myData.aFloat));
 
-    //close file
-    fs.close();
+        //close file
+        fs.close();
+    }
 
     return 0;
 }
