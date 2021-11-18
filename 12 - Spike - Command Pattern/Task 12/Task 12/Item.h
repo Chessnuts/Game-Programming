fef56bfffb5f9b35fd19cf91c24089d944058443
Ryan <102564760@student.swin.edu.au>
2021-11-18 13:13:26 +1100

    Task 12 Item and Inventory

---
+++ 12 - Spike - Command Pattern/Task 12/Task 12/Item.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+#include "GameObject.h"
+
+using namespace std;
+
+class Item : public GameObject
+{
+private:
+
+public:
+	Item(vector<string> ids, string n, string d);
+	~Item() {};
+
+};
