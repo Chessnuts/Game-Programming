7c44a1f6c79899925b5f9ba4d03ebd54ba5b214e
Ryan <102564760@student.swin.edu.au>
2021-11-23 02:08:38 +1100

    Task 16 Project setup

---
+++ 16 - Spike - Sound Board/Sound Board/Sound Board/Sound Board.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <SDL.h>
+
+using namespace std;
+
+const int WIDTH = 600;
+const int HEIGHT = 800;
+bool running = true;
+
+int main(int argc, char* args[])
+{
+    
+}
