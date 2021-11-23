12447ad1f9b4b56c8217a1942f7230845eef4fc1
Ryan <102564760@student.swin.edu.au>
2021-11-24 00:53:12 +1100

    Task 19 setup

---
+++ 19 - Spike - Messaging_ Annoucements and Blackboards/Messaging/Messaging/Messaging.cpp
@@ -0,0 +1,17 @@
+// Messaging.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    bool running = true;
+
+
+    while (running)
+    {
+
+    }
+    return 0;
+}
+
