023f27ed9af57493ca5b41958dd8c0186459602d
Ryan <102564760@student.swin.edu.au>
2021-11-23 15:24:17 +1100

    Task 17 project setup

---
+++ 17 - Spike - Sprites and Graphics/Task 17/Task 17/Task 17.cpp
@@ -0,0 +1,13 @@
+// Task 17.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+
+int main()
+{
+    
+    return 0;
+}
+
